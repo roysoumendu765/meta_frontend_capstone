1178ebaf7001c63e2a4ad1638227908a82bb5cf5
Soumendu Roy <roysoumendu765@gmail.com>
2024-03-06 18:54:28 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Nav from './components/Nav'
+import './App.css';
+import Main from './components/Main';
+import Menu from './components/Menu';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Main />
+      <Menu />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+import logo from '../images/Logo .svg'
+
+const Nav = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen)
+    }
+
+  return (
+    <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+        <a href='/' className="logo">
+            <img src={logo} alt='logo'/>
+        </a>
+
+        <div className='menu-icon' onClick={toggleMenu}>
+            <div className='bar'></div>
+            <div className='bar'></div>
+            <div className='bar'></div>
+        </div>
+
+        <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
+            <li>
+                <a href='/'>Home</a>
+                <a href='/'>About</a>
+                <a href='/'>Services</a>
+                <a href='/'>Menu</a>
+                <a href='/'>Reservations</a>
+                <a href='/'>Order Online</a>
+                <a href='/'>Login</a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
